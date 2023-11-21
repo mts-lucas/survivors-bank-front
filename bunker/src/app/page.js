99800355e11b6ddc42745618a3022570116fbc45
Lucas Mateus <lmateus1067@outlook.com>
2023-11-21 16:20:52 -0300

    feat: add dados mockados #3

--- bunker/src/app/page.js
+++ bunker/src/app/page.js
@@ -1,6 +1,20 @@
 import React from "react"
 import { MyCard } from "./components/card"
 
+const dados = {
+  "id": 1,
+  "author": {
+      "id": 1,
+      "avatar": null,
+      "nickname": "admderespeito"
+  },
+  "author_comment": "See use memory power decide. Sister machine region decade unit own. Simple century section total party may environmental fly.",
+  "cover": "/media/survivors/2023/11/16/italo.jpg",
+  "created_at": "2023-11-16T02:29:40.089093Z",
+  "name": "John Spencer",
+  "codename": "brian84",
+};
+
 export default function Home() {
   return (
     <div className="bg-black">
