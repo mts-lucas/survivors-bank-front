ac9fb2a4edfd2c02cbc71af3f1550d37727e188b
Lucas Mateus <lmateus1067@outlook.com>
2023-11-21 13:54:49 -0300

    feat: change style #3

--- bunker/src/app/components/card.js
+++ bunker/src/app/components/card.js
@@ -2,34 +2,32 @@ import React from 'react';
 
 export function MyCard() {
   return (
-    <div className="p-4 items-center justify-center w-[680px] rounded-xl group sm:flex space-x-6 bg-yellow-100 bg-opacity-90 shadow-xl hover:rounded-2xl">
+    <div className="p-4 items-center justify-center w-11/12 max-w-xl rounded-xl group sm:flex space-x-6 bg-yellow-100 bg-opacity-90 shadow-xl hover:rounded-2xl mx-auto">
       <img
-        className="mx-auto w-full block w-4/12 h-40 rounded-lg"
+        className="mx-auto block w-4/12 h-40 rounded-lg"
         alt="art cover"
         loading="lazy"
         src="https://picsum.photos/seed/2/2000/1000"
       />
-      <div className="sm:w-8/12 pl-0 p-5">
-        <div className="space-y-2">
-          <div className="space-y-4">
-            <h4 className="text-md font-semibold text-cyan-900 text-justify">
+      <div className="sm:w-8/12 pl-0">
+        <div className="space-y-2 h-40 flex flex-col pr-3">
+            <h4 className="text-md font-semibold text-red-700 text-justify">
               Title.
             </h4>
-          </div>
-          <div className="flex items-center space-x-4 justify-between">
-            <div className="flex gap-3 space-y-1">
+          <div className="flex items-center space-x-4 flex-1">
+            
               <img
                 src="https://flowbite.com/docs/images/people/profile-picture-1.jpg"
                 className="rounded-full h-8 w-8"
                 alt="author"
               />
-              <span className="text-sm">Author comment edfhdfkjhdfiehfuiefhi</span>
-            </div>
+              <span className="text-sm overflow-hidden text-ellipsis whitespace-nowrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias repudiandae mollitia adipisci libero ipsam incidunt quam? Iusto sed illum placeat beatae deserunt quod aliquid, laboriosam perferendis distinctio atque nihil sunt.</span>
+
           </div>
-          <div className="flex items-center space-x-4 justify-between">
-            <div className="text-grey-500 flex flex-row space-x-1 my-4">
-              <p className="text-xs">2 hours ago</p>
-            </div>
+          <div className="flex items-center justify-between">
+           
+              <p className="text-xs text-gray-500">2 hours ago</p>
+              <a className='bg-red-700 p-2 text-yellow-200 text-xs rounded-xl' href="*">Ver mais</a>
           </div>
         </div>
       </div>
