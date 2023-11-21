bf9365715cf6ed03913f75b6cdc3c4b2d0bb1f5f
Lucas Mateus <lmateus1067@outlook.com>
2023-11-21 16:13:13 -0300

    style: change style #3

--- bunker/src/app/components/card.js
+++ bunker/src/app/components/card.js
@@ -4,34 +4,34 @@ export function MyCard() {
   return (
     <div className="p-4 items-center justify-center w-11/12 max-w-xl rounded-xl group sm:flex space-x-6 bg-yellow-100 bg-opacity-90 shadow-xl hover:rounded-2xl mx-auto">
       <img
-        className="mx-auto block w-4/12 h-40 rounded-lg"
+        className="mx-auto block w-4/12 h-40 rounded-2xl"
         alt="art cover"
         loading="lazy"
         src="https://picsum.photos/seed/2/2000/1000"
       />
       <div className="sm:w-8/12 pl-0">
         <div className="space-y-2 h-40 flex flex-col pr-3">
-            <h4 className="text-md font-semibold text-red-700 text-justify">
-              Title.
+            <h4 className="text-md font-semibold text-red-700 text-justify underline">
+              Zezinho - O bandoleiro
             </h4>
           <div className="flex items-center space-x-4 flex-1">
-            
-              <img
-                src="https://flowbite.com/docs/images/people/profile-picture-1.jpg"
-                className="rounded-full h-8 w-8"
-                alt="author"
-              />
               <span className="text-sm overflow-hidden text-ellipsis whitespace-nowrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias repudiandae mollitia adipisci libero ipsam incidunt quam? Iusto sed illum placeat beatae deserunt quod aliquid, laboriosam perferendis distinctio atque nihil sunt.</span>
 
           </div>
           <div className="flex items-center justify-between">
            
               <p className="text-xs text-gray-500">2 hours ago</p>
-              <a className='bg-red-700 p-2 text-yellow-200 text-xs rounded-xl' href="*">Ver mais</a>
+              <a className='bg-red-700 p-2 text-yellow-200 text-xs rounded-xl flex flex-row' href="*">
+                <img
+                  src="https://flowbite.com/docs/images/people/profile-picture-1.jpg"
+                  className="rounded-full h-1/4 w-4 mr-2"
+                  alt="author"
+                />
+                  Nikcnme
+              </a>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
