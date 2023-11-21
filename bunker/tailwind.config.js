2d3dcf0f0464e3ddbcf076ae0d07477d4a832e35
italo <italomauricio98@gmail.com>
2023-11-21 01:54:43 -0300

    subindo código teste

--- bunker/tailwind.config.js
+++ bunker/tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'h-gray-500': '#737994',
+      'h-blue-500': '#8CAAEE',
+      'h-blue-900': '#303446',
+    },
+    width: {
+      120: '30rem',
+      125: '31.25rem'
+    }
   },
   plugins: [],
 }
