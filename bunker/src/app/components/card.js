6d206f392829365b242aabf6dc1f02f4325c4671
Lucas Mateus <lmateus1067@outlook.com>
2023-11-22 10:19:45 -0300

    feat? finzaliando card #3

--- bunker/src/app/components/card.js
+++ bunker/src/app/components/card.js
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 export function MyCard({ sheet }) {
   return (
-    <div className='flex flex-row justify-between my-2 bg-yellow-100 rounded-xl border-2 border-green-500 p-2'>
+    <div className='flex flex-row justify-start my-2 bg-yellow-100 rounded-xl border-2 border-solid border-red-100 p-2'>
       <div>
-        <img className='min-w-36 h-32 rounded-xl object-cover' src="https://img.olhardigital.com.br/wp-content/uploads/2023/08/pokemon-anime.jpg" alt="" />
+        <img className='min-w-36 h-32 rounded-xl object-cover' src={sheet.cover} alt="cover" />
       </div>
-      <div className='ml-2 space-y-4'>
+      <div className='ml-2 space-y-4 flex flex-col max-w-[80%] overflow-hidden overflow-ellipsis'>
         <Link className='text-red-700 font-semibold text-lg' href={'/survivors'}>{sheet.name} {sheet.codename}</Link>
         <p className='flex flex-wrap text-justify'>{sheet.author_comment}</p>
         <p className='text-gray-500'>{sheet.created_at}</p>
@@ -15,3 +15,6 @@ export function MyCard({ sheet }) {
     </div>
   );
 }
+
+
+
--- bunker/src/app/components/cardList.js
+++ bunker/src/app/components/cardList.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { MyCard } from "./card";
 
-export function CardList({dados}){
+const dados = {
+    "id": 1,
+    "author": {
+        "id": 1,
+        "avatar": null,
+        "nickname": "admderespeito"
+    },
+    "author_comment": "See use memory power decide. Sister machine region decade unit own. Simple century section total party may environmental",
+    "cover": "https://img.olhardigital.com.br/wp-content/uploads/2023/08/pokemon-anime.jpg",
+    "created_at": "2023-11-16T02:29:40.089093Z",
+    "name": "John Spencer",
+    "codename": "brian84",
+  };
+
+export function CardList(){
 
     return(
-        <div className="border-2 border-black  flex flex-col justify-center w-9/12 m-auto">
+        <div className="flex flex-col justify-center w-9/12 m-auto">
             <MyCard sheet={dados}/>
             <MyCard sheet={dados}/>
             <MyCard sheet={dados}/>
