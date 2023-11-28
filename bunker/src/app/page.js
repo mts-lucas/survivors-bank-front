c2d61964705457cb6510f7dc619802327ee9c9b5
italo <italomauricio98@gmail.com>
2023-11-27 21:32:19 -0300

    remove image in the page

--- bunker/src/app/page.js
+++ bunker/src/app/page.js
@@ -4,14 +4,6 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen relative">
-      <div className="bg-cover bg-center w-full h-full absolute z-0">
-        <Image 
-          src='/spock-zombie.svg'  
-          alt='test'
-          layout='fill'
-          objectFit='cover'
-        />
-      </div>
       <h1 className="text-3xl font-bold underline z-10 text-white">
         Hello World
       </h1>
