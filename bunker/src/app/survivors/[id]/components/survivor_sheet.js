a56090afa6f932120316752fedfa8754a19c2e28
italo <italomauricio98@gmail.com>
2023-12-02 15:54:22 -0300

    adicionando componente pagination

--- bunker/src/app/survivors/[id]/components/survivor_sheet.js
+++ bunker/src/app/survivors/[id]/components/survivor_sheet.js
@@ -1,12 +1,43 @@
-import React from "react";
+'use client';
+import React, { useState, useEffect } from "react";
+import Pagination from "../../../components/pagination";
+import axios from "axios";
+
+
 
 export function SurvivorSheet({ sheet }) {
+
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [survivorData, setSurvivorData] = useState({});
+  
+
+  // const handlePageChange = (pageNumber) => {
+  //   setCurrentPage(pageNumber);
+  // };
+
+  // const fetchSurvivorData = async (pageNumber) => {
+  //   try {
+  //     const response = await axios.get(`https://bunkerapi.onrender.com/bunker/api/v1/survivors/?page=${pageNumber}`);
+  //     console.log(response);
+  //     const data = response.data.results.length > 0 ? response.data.results[0] : null;
+      
+  //     setSurvivorData(data);
+  //   } catch (error) {
+  //     console.error("Erro ao buscar dados da API:", error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchSurvivorData(currentPage);
+  // }, [currentPage]);
+
+  // const totalPages = 10;
   return (
     <div className="bg-red-800 border-solid border-yellow-100 border-4 rounded-3xl flex flex-col text-yellow-100 p-4 md:p-8 mt-8 mb-16 max-w-screen-xl mx-auto overflow-y-auto">
       <div className="container mx-auto overflow-y-auto" style={{ maxHeight: 'calc(100vh - 100px)' }}>
         <div className="flex flex-col md:flex-row justify-around items-center">
           <div className="mb-4 md:m-8">
-            <h1 className="font-bold text-4xl md:text-6xl text-yellow-100">{sheet.name}</h1>
+            <h1 className="font-bold text-4xl md:text-6xl text-yellow-100">{survivorData.name}</h1>
           </div>
           <div className="mb-4 md:m-8">
             <img
@@ -22,16 +53,17 @@ export function SurvivorSheet({ sheet }) {
         </div>
         <div className="flex flex-col items-center border-2 rounded-xl border-dashed border-yellow-100 p-4 md:p-8 m-4 md:m-8">
           <h1 className="font-bold text-3xl md:text-4xl my-4 md:my-8 text-yellow-100">Torments</h1>
-          <p className="text-lg md:text-2xl text-yellow-100">{sheet.torments}</p>
+          <p className="text-lg md:text-2xl text-yellow-100">{survivorData.torments}</p>
         </div>
         <div className="flex flex-col items-center border-2 rounded-xl border-dashed border-yellow-100 p-4 md:p-8 m-4 md:m-8">
           <h1 className="font-bold text-3xl md:text-4xl my-4 md:my-8 text-yellow-100">Conditions</h1>
-          <p className="text-lg md:text-2xl text-yellow-100">{sheet.conditions}</p>
+          <p className="text-lg md:text-2xl text-yellow-100">{survivorData.conditions}</p>
         </div>
         <div className="flex flex-col items-center border-2 rounded-xl border-dashed border-yellow-100 p-4 md:p-8 m-4 md:m-8">
           <h1 className="font-bold text-3xl md:text-4xl my-4 md:my-8 text-yellow-100"></h1>
-          <p className="text-lg md:text-2xl text-yellow-100">{sheet.conviction}</p>
+          <p className="text-lg md:text-2xl text-yellow-100">{survivorData.conviction}</p>
         </div>
+        <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={handlePageChange} />
       </div>
     </div>
   );
