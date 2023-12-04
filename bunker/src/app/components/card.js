b520439f1e2e2b96d170a5cb4767dced2159f0c3
italo <italomauricio98@gmail.com>
2023-12-03 23:21:13 -0300

    update: carlist and card issue#23

--- bunker/src/app/components/card.js
+++ bunker/src/app/components/card.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
-export function MyCard({ sheet }) {
+export function MyCard({ sheet, url }) {
   return (
     <div className='flex flex-row justify-start my-2 bg-yellow-100 rounded-xl border-2 border-solid border-red-100 p-2'>
       <div>
-        <img className='min-w-36 h-32 rounded-xl object-cover' src={sheet.cover} alt="cover" />
+        <Image
+          className='min-w-36 h-32 rounded-xl object-cover'
+          src={`/assets/${url}-avatar.svg`}
+          alt="cover"
+          width={144}
+          height={128}
+        />
       </div>
       <div className='ml-2 space-y-4 flex flex-col max-w-[80%] overflow-hidden overflow-ellipsis'>
-        <Link className='text-red-700 font-semibold text-lg' href={'/survivors'}>{sheet.name} {sheet.codename}</Link>
+        <Link className='text-red-700 font-semibold text-lg' href={`/${url}s/${sheet.id}`}>{sheet.name} {sheet.codename}</Link>
         <p className='flex flex-wrap text-justify'>{sheet.author_comment}</p>
         <p className='text-gray-500'>{sheet.created_at}</p>
       </div>
@@ -16,5 +23,20 @@ export function MyCard({ sheet }) {
   );
 }
 
+// import React from 'react';
+// import Link from 'next/link';
 
-
+// export function MyCard({ sheet }) {
+//   return (
+//     <div className='flex flex-row justify-start my-2 bg-yellow-100 rounded-xl border-2 border-solid border-red-100 p-2'>
+//       <div>
+//         <img className='min-w-36 h-32 rounded-xl object-cover' src={sheet.cover} alt="cover" />
+//       </div>
+//       <div className='ml-2 space-y-4 flex flex-col max-w-[80%] overflow-hidden overflow-ellipsis'>
+//         <Link className='text-red-700 font-semibold text-lg' href={'/survivors'}>{sheet.name} {sheet.codename}</Link>
+//         <p className='flex flex-wrap text-justify'>{sheet.author_comment}</p>
+//         <p className='text-gray-500'>{sheet.created_at}</p>
+//       </div>
+//     </div>
+//   );
+// }
--- bunker/src/app/monsters/[id]/components/monster_sheet.js
+++ bunker/src/app/monsters/[id]/components/monster_sheet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from 'next/image';
 
 export function MonsterSheet({ sheet }) {
   return (
@@ -8,10 +9,12 @@ export function MonsterSheet({ sheet }) {
           <h1 className="font-bold text-4xl md:text-6xl text-red-500">{sheet.name}</h1>
         </div>
         <div className="mb-4 md:m-8">
-          <img
-            src="https://img.olhardigital.com.br/wp-content/uploads/2023/08/pokemon-anime.jpg"
-            alt="cover"
-            className="w-full md:min-w-36 md:max-w-xl rounded-xl border-2 p-2 border-red-500 "
+          <Image
+              src="/assets/monster-avatar.svg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+              className="w-full md:min-w-36 md:max-w-xl rounded-xl border-2 p-2 border-red-500 "
           />
         </div>
       </div>
