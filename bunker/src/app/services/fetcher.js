ee57bbbe40121b31b26694414001280c08b8ba96
italo <italomauricio98@gmail.com>
2023-12-05 23:33:48 -0300

    removendo try catch

--- bunker/src/app/services/fetcher.js
+++ bunker/src/app/services/fetcher.js
@@ -1,16 +1,9 @@
 
 export async function fetcher(url) {
-    try{
 
         const res = await fetch(url);
 
         const json = await res.json();
     
         return json;
-    }catch(err){
-
-        console.log(err);
-        
-    }
-
 }
