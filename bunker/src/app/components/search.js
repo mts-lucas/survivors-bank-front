48bb7b32f687814df727b76444ee2fd6ad5e370e
italo <italomauricio98@gmail.com>
2023-12-07 02:06:14 -0300

    create menu search #29

--- bunker/src/app/components/search.js
+++ bunker/src/app/components/search.js
@@ -1,37 +1,17 @@
-// components/Menu.js
-"use client";
 import { useState } from 'react';
 
-export function Search() {
-  const [searchQuery, setSearchQuery] = useState('');
+export default function Search({ data, onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const handleInputChange = (e) => {
-    setSearchQuery(e.target.value);
-  };
-
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    // Aqui você pode adicionar a lógica para lidar com a pesquisa, por exemplo, redirecionar para uma página de resultados.
-    console.log('Search submitted:', searchQuery);
+  const handleSearch = () => {
+    const results = data.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    onSearch(results);
   };
 
   return (
-    <form onSubmit={handleSearchSubmit} className="flex items-center">
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchQuery}
-        onChange={handleInputChange}
-        className="border border-gray-300 px-2 py-1 mr-2 rounded-md"
-      />
-      <button
-        type="submit"
-        className="bg-red-700 text-white px-3 py-1 rounded-md"
-      >
-        Search
-      </button>
-    </form>
+    <div>
+      <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="border p-2 rounded text-2xl" />
+      <button onClick={handleSearch} className="bg-red-600 text-yellow-100 text-3xl px-4 py-2 rounded ml-2">Search</button>
+    </div>
   );
-};
-
-
+}
--- bunker/src/app/monsters/page.js
+++ bunker/src/app/monsters/page.js
@@ -3,30 +3,66 @@ import { useState, useEffect } from 'react';
 import { CardList } from "../components/cardList";
 import { fetcher } from "../services/fetcher";
 import Pagination from "../components/pagination";
+import Search from "../components/search";
 
 export default function Page() {
   const [currentPage, setCurrentPage] = useState(1);
   const [data, setData] = useState([]);
+  const [allData, setAllData] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
 
-  const fetchPageData = async (pageNumber) => {
-    const url = `https://bunkerapi.onrender.com/bunker/api/v1/monsters/?page=${pageNumber}`;
-    const newData = await fetcher(url);
-    setData(newData.results);
+  const fetchAllData = async () => {
+    let allData = [];
+    for (let i = 1; i <= 6; i++) {
+      const url = `https://bunkerapi.onrender.com/bunker/api/v1/monsters/?page=${i}`;
+      const newData = await fetcher(url);
+      allData = [...allData, ...newData.results];
+    }
+    setAllData(allData);
+    setData(allData.slice(0, 10));
   };
 
   const onPageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
-    fetchPageData(pageNumber);
+    setData((isSearching ? searchResults : allData).slice((pageNumber - 1) * 10, pageNumber * 10));
+  };
+
+  const handleSearch = (results) => {
+    setSearchResults(results);
+    setIsSearching(true);
+    setCurrentPage(1);
+    setData(results.slice(0, 10));
+  };
+
+  const resetSearch = () => {
+    setIsSearching(false);
+    setSearchResults([]);
+    setData(allData.slice(0, 10));
   };
 
   useEffect(() => {
-    fetchPageData(1);
+    fetchAllData();
   }, []);
+
+  const totalPages = Math.ceil((isSearching ? searchResults.length : allData.length) / 10);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <CardList data={data} url={"monster"} />
-      <Pagination totalPages={6} currentPage={currentPage} onPageChange={onPageChange} />
+      <Search data={allData} onSearch={handleSearch} />
+      {data.length === 0 && (
+        <p className='text-5xl text-white flex items-center p-10'>
+          No results found.
+        </p>
+      )}
+      {data.length > 0 && (
+        <>
+          <CardList data={data} url={"monster"} />
+          {totalPages > 1 && (
+            <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={onPageChange} />
+          )}
+        </>
+      )}
     </div>
   );
 }
