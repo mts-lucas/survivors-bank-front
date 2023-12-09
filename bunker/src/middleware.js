860b1bdb6b943333b1f27b598f46c0c278333901
Lucas Mateus <lmateus1067@outlook.com>
2023-12-09 15:08:46 -0300

    feat: add middleware #28

---
+++ bunker/src/middleware.js
@@ -0,0 +1,7 @@
+export { default } from "next-auth/middleware"
+
+
+// adicionar as urs que forem privadas, tirar dashboard e por a primeira, as proximas é so adicioanr no array
+// caso queira privar uma sessoao inteira seguir como esta em:
+// https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+export const config = { matcher: ["/dashboard"] }
