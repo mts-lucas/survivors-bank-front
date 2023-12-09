8214408f826a125ab91dfb75673d1135e4f2e927
Lucas Mateus <lmateus1067@outlook.com>
2023-12-09 13:07:31 -0300

    feat: finalizando autenticação

--- bunker/src/app/api/auth/[...nextauth]/route.js
+++ bunker/src/app/api/auth/[...nextauth]/route.js
@@ -1,66 +1,70 @@
 import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import jwt from "next-auth/jwt";
+import { nextAuthOptions } from "../../../utils/authOptions"
+// import CredentialsProvider from "next-auth/providers/credentials"
+// import jwt from "next-auth/jwt";
 
 
-const nextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-          name: 'BunkerAPI',
-          credentials: {
-            username: { label: "username", type: "text"},
-            password: { label: "password", type: "password" }
-          },
-          async authorize(credentials, req) {
-            const res = await fetch("https://bunkerapi.onrender.com/bunker/api/v1/login/", {
-                method: 'POST',
-                body: JSON.stringify({
-                    username: credentials?.username,
-                    password: credentials?.password,
-            }),
-                headers: { "Content-Type": "application/json" }
-            })
 
-            const user = await res.json();
+// const nextAuthOptions = {
+//     providers: [
+//         CredentialsProvider({
+//           name: 'BunkerAPI',
+//           credentials: {
+//             username: { label: "username", type: "text"},
+//             password: { label: "password", type: "password" }
+//           },
+//           async authorize(credentials, req) {
+//             const res = await fetch("https://bunkerapi.onrender.com/bunker/api/v1/login/", {
+//                 method: 'POST',
+//                 body: JSON.stringify({
+//                     username: credentials?.username,
+//                     password: credentials?.password,
+//             }),
+//                 headers: { "Content-Type": "application/json" }
+//             })
+
+//             const user = await res.json();
     
-            if (res.ok && user) {
-                return user
-            }
-            return null
-          }
-        })
-      ],
-    callbacks: {
-      async jwt({ token, user, account, profile, isNewUser}) {
+//             if (res.ok && user) {
+//                 return user
+//             }
+//             return null
+//           }
+//         })
+//       ],
+//     callbacks: {
+//       async jwt({ token, user, account, profile, isNewUser}) {
     
-        if (user) {
-          token.accessToken = user.access
-        }
+//         if (user) {
+//           token.accessToken = user.access
+//         }
 
-        return token
-      },
+//         return token
+//       },
 
-      async session({session, token}){
+//       async session({session, token}){
 
-        session.accessToken = token.accessToken;
-        const API_URL = 'https://bunkerapi.onrender.com/bunker/api/v1/';
+//         session.accessToken = token.accessToken;
+//         const API_URL = 'https://bunkerapi.onrender.com/bunker/api/v1/';
 
-        const config = {
-          headers: { Authorization: `Bearer ${token.accessToken}` },
-          method: 'GET',
-        };
+//         const config = {
+//           headers: { Authorization: `Bearer ${token.accessToken}` },
+//           method: 'GET',
+//         };
 
-        let userData = await fetch(`${API_URL}whoiam`, config)
+//         let userData = await fetch(`${API_URL}whoiam`, config)
 
-        session.user = await userData.json()
+//         session.user = await userData.json()
 
-        return session;
+//         return session;
 
-      },
-    }
+//       },
+//     },
+//     session: { strategy: "jwt" },
+//     secret: process.env.NEXTAUTH_SECRET,
 
-}
+// }
 
 const handler = NextAuth(nextAuthOptions)
   
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST}
--- bunker/src/app/layout.js
+++ bunker/src/app/layout.js
@@ -1,31 +1,34 @@
 // components/RootLayout.js
-"use client"
 import React from 'react';
 import './globals.css';
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 import { Navbar } from './components/navbar';
-import { SessionProvider } from 'next-auth/react';
+import Provider from "./context/client-provider"
+import { getServerSession } from "next-auth/next"
+// import { nextAuthOptions } from "./api/auth/[...nextauth]/route"
+import { nextAuthOptions } from "./utils/authOptions"
 
-// export const metadata = {
-//   title: 'Bunker',
-//   description: 'Pweb project',
-// }
+export const metadata = {
+  title: 'Bunker',
+  description: 'Pweb project',
+}
 
-const RootLayout = ({ children }) => {
+export default async function RootLayout ({ children }){
   const backgroundImage = '/spock-zombie.svg'; 
+  const session = await getServerSession(nextAuthOptions)
 
   return (
     <html lang="en" className="h-full">
       <body className="flex flex-col min-h-screen" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
-        < SessionProvider>
+        <Provider session={session}>
           <Header />
           <div className="flex justify-center">
             <Navbar />
           </div>
           <div className="flex-grow">{children}</div>
           <Footer />
-        </SessionProvider>
+        </Provider>
         {/* <Header />
         <div className="flex justify-center">
           <Navbar />
@@ -36,5 +39,3 @@ const RootLayout = ({ children }) => {
     </html>
   );
 }
-
-export default RootLayout;
